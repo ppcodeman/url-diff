021df22f69533fe9032f36b081e07b64f5f45953
Paul Duke <paul@morethanmiltoncan.com>
2013-09-28 19:59:58 -0400

    remove old commented out code, add some TODOs, make printing prettier. add method to normalize a URL

--- url_params.py
+++ url_params.py
@@ -7,51 +7,24 @@ A detailed description of url_params.
 import argparse
 import sys
 
-# def get_params(url):
-#   """Returns a dict of the url params."""
-#   param_dict = {}
-#   if '?' not in url:
-#     return param_dict
-#   params_pos = url.find('?') + 1
-#   for token in url[params_pos:].split('&'):
-#     if '=' not in token:
-#       continue
-#     partitioned_param = token.partition('=')
-#     param_dict[partitioned_param[0]] = partitioned_param[2]
-#   return param_dict
-
-# def diff_params(left_params, right_params, names_only=False):
-#     for left_key in left_params.iterkeys():
-#       if left_key in right_params:
-#         if left_params[left_key] != right_params[left_key]:
-#           print left_key
-#           if not names_only;
-#             print '< %s' % (left_params[left_key])
-#             print '> %s\n' % (right_params[left_key])
-#       else:
-#         print left_key
-#         if not names_only:
-#           print '< %s\n' % (left_params[left_key])
-
-#   for right_key in right_params.iterkeys():
-#     if right_key not in left_params:
-#       print right_key
-#       if right_params[right_key] != left_params[right_key]:
-#         print '< %s' % (left_params[right_key])
-#         print '> %s\n' % (right_params[right_key])
-#       print
-
-class Url_Differ():
+class Url_Differ(object):
 
   PATH_DELIM = '?'
   PARAM_DELIM = '&'
   NAME_VAL_DELIM = '='
 
   def __init__(self, left_url, right_url, names_only=False):
-    self._left_url = left_url
-    self._right_url = right_url
+    self._left_url = self._normalize_url(left_url)
+    self._right_url = self._normalize_url(right_url)
     self._names_only = names_only
 
+  def _normalize_url(self, url):
+    """Strips white space, and removes all chars after #"""
+    ret = url.strip()
+    if '#' in ret:
+      idx = ret.index('#')
+      ret = ret[:idx]
+    return ret
 
   def _get_params(self, url):
     """Returns a dict of the url params."""
@@ -68,9 +41,7 @@ class Url_Differ():
 
   def _diff_params(self, left_params, right_params, names_only=False):
     """Returns a list of the diffence between dicts on key/values.
-
     """
-    # TODO(macpd) put output in string instead of printing
     diffs = []
     for left_key in left_params.iterkeys():
       if left_key in right_params:
@@ -79,16 +50,19 @@ class Url_Differ():
           if not names_only:
             diffs.append('< %s' % (left_params[left_key]))
             diffs.append('> %s' % (right_params[left_key]))
+          diffs.append('')
       else:
         diffs.append(left_key)
         if not names_only:
           diffs.append('< %s' % (left_params[left_key]))
+        diffs.append('')
 
     for right_key in right_params.iterkeys():
       if right_key not in left_params:
         diffs.append(right_key)
         if not names_only:
           diffs.append('> %s' % (right_params[right_key]))
+        diffs.append('')
 
     return diffs
 
@@ -111,11 +85,12 @@ def print_usage(self):
 def main():
   # TODO(macpd): main function documentation
   # TODO(macpd): usage string
+  # TODO(macpd): provide option to url decode params before comparison
+  # TODO(macpd): provide option to diff case insensitively
   arg_parser = argparse.ArgumentParser()
   # TODO(macpd): match domain and hostname
   arg_parser.add_argument('--hostname', default=False, required=False,
       help='also diff URL hostname', action='store_true', dest='diff_hostname')
-  # TODO(macpd): match URL param names
   arg_parser.add_argument('-n', '--names', default=False, required=False,
       help='only diff URL parameter names.', action='store_true', dest='names_only')
   arg_parser.add_argument('left_url', type=str, help='URL to diff against.  logically hadled as the left argurmnt of diff.', metavar='<left URL>')
@@ -123,12 +98,7 @@ def main():
 
   args = arg_parser.parse_args()
 
-
-  # if len(sys.argv) != 2:
-  #   print_usage()
-  #   sys.exit(-1)
-
-  differ = Url_Differ(args.left_url, args.right_url, args.names_only)
+  differ = Url_Differ(args.left_url, args.right_url, names_only=args.names_only)
   print differ
   # differ.print_diff()
 
