efcb0a2fbd51460416f4a1409f020f52eb2d166d
Paul Duke <paul@morethanmiltoncan.com>
2013-09-29 18:53:20 -0400

    added hostname diffing of URLs. additional documentation.

--- url_params.py
+++ url_params.py
@@ -17,6 +17,10 @@ class ParamDiffTypeError(Error):
   """Raised when an incorrect diff type used."""
   pass
 
+class HostnameParseError(Error):
+  """Raised when unable to parse hostname from URL."""
+  pass
+
 class ParamDiffEntry(object):
   """Represents difference of 2 URL params with same name."""
   LEFT_ONLY = 1
@@ -46,18 +50,6 @@ class ParamDiffEntry(object):
   def name(self):
     return self._name
 
-  @property
-  def left_val(self):
-    return self._left_val
-
-  @property
-  def right_val(self):
-    return self._right_val
-
-  @property
-  def diff_type(self):
-    return self._type
-
   def __str__(self):
     ret = self._name
     if self._type == self.LEFT_ONLY or self._type == self.BOTH_DIFFER:
@@ -73,12 +65,15 @@ class UrlDiffer(object):
   PATH_DELIM = '?'
   PARAM_DELIM = '&'
   NAME_VAL_DELIM = '='
+  SCHEME_DELIM = '://'
 
 
   def __init__(self, left_url, right_url, names_only=False, hostnames=False):
     self._left_url = self._normalize_url(left_url)
     self._right_url = self._normalize_url(right_url)
     self._names_only = names_only
+    self._wants_hostname_diff = hostnames
+    self._diffs = []
     self._do_diff()
 
   def __str__(self):
@@ -99,6 +94,40 @@ class UrlDiffer(object):
       ret = ret[:idx]
     return ret
 
+  def _get_hostname(self, url):
+    """Parses the hostname from a URL."""
+    try:
+      scheme_idx = url.find(self.SCHEME_DELIM)
+      if scheme_idx == -1:
+        hostname_begin = 0
+        if not url[hostname_begin].isalnum():
+          raise ValueError
+      else:
+        hostname_begin = scheme_idx + len(self.SCHEME_DELIM)
+      hostname_end = url.index('/', hostname_begin)
+      return url[hostname_begin:hostname_end]
+    except ValueError:
+      logging.error('Unable to parse hostname from %s', url)
+      raise HostnameParseError
+
+  def _diff_hostnames(self, left, right):
+    """Diffs hostnames, if different appends ParamDiffEntry to diffs list.
+
+    Args:
+      left: String; left hostname.
+      right: String; right hostname.
+    Returns:
+      Bool; True if different, else False.
+    """
+    if left == right:
+      self._hostnames_differ = False
+    else:
+      self._hostnames_differ = True
+      self._diffs.append(ParamDiffEntry('Hostname', left, right,
+          ParamDiffEntry.BOTH_DIFFER))
+
+    return self._hostnames_differ
+
   def _get_params(self, url):
     """Returns a dict of the url params."""
     param_dict = {}
@@ -114,6 +143,13 @@ class UrlDiffer(object):
 
   def _diff_params(self, left_params, right_params):
     """Returns a list of the diffence between dicts on key/values.
+
+    Args:
+      left_param: dict; param name -> value dict of the left URL.
+      right_param: dict; param name -> value dict of the right URL.
+
+    Returns:
+      dict of ParamDiffEntry of differences between the left and right params.
     """
     diffs = []
     for left_key in left_params.iterkeys():
@@ -134,9 +170,14 @@ class UrlDiffer(object):
     return diffs
 
   def _do_diff(self):
+    if self._wants_hostname_diff:
+      self._left_hostname = self._get_hostname(self._left_url)
+      self._right_hostname = self._get_hostname(self._right_url)
+      self._diff_hostnames(self._left_hostname, self._right_hostname)
     self._left_params_dict = self._get_params(self._left_url)
     self._right_params_dict = self._get_params(self._right_url)
-    self._diffs = self._diff_params(self._left_params_dict, self._right_params_dict)
+    self._diffs.extend(self._diff_params(
+      self._left_params_dict, self._right_params_dict))
 
   def left_params(self):
     """Returns a deep coy of the left params dict."""
